ad54851fc322e5c0b5ad1c9b443087d4c04bd22a
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-12 17:04:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,13 +1,13 @@
 
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  counter = 1   
+  counter = 0    
   loop do
+    counter += 1 
     puts phrase
     if counter == number_of_times
       break
     end
-    counter += 1 
   end
 end
 
